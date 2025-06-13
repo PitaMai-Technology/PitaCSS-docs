e19684fa6f245c46964ad86a3135e3930e64dfe3
Eita <kusaiyuka@gmail.com>
2025-06-13 17:37:01 +0900

    ヘッダーのスタイルを改善し、pita-cssのバージョンを0.3.13に更新

--- app/components/TheAppHeader.vue
+++ app/components/TheAppHeader.vue
@@ -1,12 +1,12 @@
 <template>
-    <header class="headerContainer">
-        <div class="logoContainer hide-max1250">
-            <NuxtLink to="/" class="inherit-link">
+    <header>
+        <div class="logo-container hide-max1250">
+            <NuxtLink to="/" class="inherit-link logo-text">
                 PitaCSS(α)
             </NuxtLink>
         </div>
-        <div class="logoContainerSp hide-min1250">
-            <NuxtLink to="/" class="inherit-link">
+        <div class="logo-container-responsive hide-min1250">
+            <NuxtLink to="/" class="inherit-link logo-text">
                 PitaCSS(α)
             </NuxtLink>
         </div>
@@ -16,42 +16,9 @@
 <script setup>
 </script>
 
-<style>
-.headerContainer {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: var(--gray-primary);
-    box-shadow: var(--shadow-lg);
-    position: sticky;
-    top: 0;
-    z-index: 1000;
-    height: 72px;
-    padding: 0 40px;
+<style scoped>
 
-    @media screen and (max-width: 1250px) {
-        padding: 0 20px;
-        justify-content: flex-end;
-    }
-}
-
-.logoContainer {
-    display: flex;
-    align-items: center;
-    max-width: 300px;
-    font-size: var(--text-xl);
-    font-weight: bold;
-}
-
-.logoContainerSp{
-    display: flex;
-    align-items: center;
-    max-width: 180px;
-    font-size: var(--text-xl);
-    font-weight: bold;
-}
-
-.logoText {
+.logo-text {
     font-size: var(--text-lg);
     font-weight: 700;
     margin: 0;
